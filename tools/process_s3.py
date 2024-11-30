017b29adf8bc4f48fa40e9d1712f44b5794bbadb
Vinicius Maciel <vinicius.nmaciel@gmail.com>
2024-11-30 03:33:14 -0300

    checkpoint

--- tools/process_s3.py
+++ tools/process_s3.py
@@ -13,6 +13,43 @@ def generate_char_declaration(tokens):
     value = value.replace(",0", "").replace("'", "\"").replace("\\", "\\\\")
     return f'char {variable_name}[] = {value};\n'
 
+def process_valid_lines(valid_lines):
+    """Processa as linhas válidas e salva no arquivo C."""
+    output = []
+    i = 0
+
+    while i < len(valid_lines):
+        tokens = split_line_to_array(valid_lines[i])
+
+        if len(tokens) == 3:
+            # Verificar o próximo item
+            if i + 1 < len(valid_lines):
+                next_tokens = split_line_to_array(valid_lines[i + 1])
+                if len(next_tokens) == 3:
+                    # Imprime o item atual e passa para o próximo
+                    c_line = generate_char_declaration(tokens)
+                    output.append(c_line)
+                    i += 1
+                    continue            
+
+            if next_tokens[0].startswith("align"):
+                # Adiciona o alinhamento
+                align_value = next_tokens[1]
+                c_line = f'__declspec(align({align_value})) {generate_char_declaration(tokens)}'
+                output.append(c_line)
+                i += 1  # Pular o próximo item (o alinhamento)
+            else:
+                # Processa o item normalmente
+                c_line = generate_char_declaration(tokens)
+                output.append(c_line)
+        else:
+            # Processa o item normalmente, sem nenhuma modificação
+            output.append(valid_lines[i] + "\n")
+
+        i += 1  # Continua para o próximo item
+
+    return output
+
 def process_asm_file():
     # Diretórios do projeto
     project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # Raiz do projeto
@@ -56,10 +93,8 @@ def process_asm_file():
     
     # Processar as linhas válidas e salvar no arquivo de saída
     with open(valid_file, "w") as valid_output:
-        for line in valid_lines:
-            tokens = split_line_to_array(line)
-            c_line = generate_char_declaration(tokens)
-            valid_output.write(c_line)
+        c_lines = process_valid_lines(valid_lines)
+        valid_output.writelines(c_lines)
 
 # Executar a função
 if __name__ == "__main__":
